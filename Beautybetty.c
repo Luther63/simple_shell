43fb2daad2148c6c2e0a6eaf5541abfc06707f66
luther63 <murangi516@gmail.com>
2023-12-04 06:08:39 -0800

    get rich soon soon

---
+++ Beautybetty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - discribes the main function
+ *
+ * Return: always  0 when succesfull
+ */
+
+int main(void)
+
+{
+	printf("i love programming,\n");
+
+	return (0);
+}
